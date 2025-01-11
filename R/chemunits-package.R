86b6fa8ac8d03cda3e86785d67ec28b1ed974981
Sebastian Kopf <sebastian.kopf@colorado.edu>
2025-01-11 01:57:40 +0100

    package defintion file

---
+++ R/chemunits-package.R
@@ -0,0 +1,19 @@
+#' @details
+#' `r lifecycle::badge("experimental")`
+#'
+#' The chemunits package augments the units package for use in chemistry calculations. 
+#'
+#' Resources:
+#'   * Website for the chemunits package: <https://chemunits.kopflab.org>
+#'   * Create a chemunit: [set_cu()]
+#'   * Package options: [chemunits_options]
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import rlang
+#' @import cli
+#' @import vctrs
+## usethis namespace: end
+NULL
+
+# note: because `chemunits` depends on `units`, all units functions are automatically available and do not need to be re-exported here
