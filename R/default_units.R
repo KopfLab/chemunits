2b72fdeadd012b0fe7792e9d029d1968f103aa8b
Sebastian Kopf <sebastian.kopf@colorado.edu>
2025-01-11 01:57:51 +0100

    draft default units function

---
+++ R/default_units.R
@@ -0,0 +1,27 @@
+#' automatically convert to default units if any fit (`defaults` would be an option in units_options)
+#' @export
+convert_to_default_units <- function(x, defaults = c("mol", "M", "L", "1")) {
+  # safety check
+  stopifnot(
+    "`x` has to have units" = !missing(x) && is(x, "units"),
+    "`defaults` has to be a scalar" = 
+      rlang::is_empty(defaults) || rlang::is_character(defaults)
+  )
+  if (rlang::is_empty(defaults)) return(x)
+  
+  # which default units are valid?
+  x_units <- base::units(x) |> as.character()
+  valid_units <- sapply(defaults, units::ud_are_convertible, x_units, USE.NAMES = FALSE)
+  
+  if (sum(valid_units) == 0L) return(x) # no default unit for this quantity
+  if (sum(valid_units) > 1L) { # more than one default unit
+    sprintf("more than one valid default unit for '%s' - will use '%s' and ignore '%s'",
+            x_units, defaults[valid_units][1], 
+            paste(defaults[valid_units][-1], collapse = "', '")) |>
+      warning(immediate. = TRUE, call. = FALSE)
+  }
+  
+  # convert to default unit
+  base::units(x) <- defaults[valid_units][1]
+  return(x)
+}
